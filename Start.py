8bb6c8a319d5930f2664b2c1c0c1bd60ac49b4d5
andreyfly63 <andrey63ru@mail.ru>
2019-01-12 15:21:51 +0400

    release v2(add second argument in get requast)

--- Start.py
+++ Start.py
@@ -14,11 +14,13 @@ app = Flask(__name__)
 def test():
     try:
         user = request.args.get('name')
+        year = int(request.args.get('year'))
         pattern = r'[^a-zA-Z]+'
         if re.findall(pattern, user):
             raise TypeError("U write uncorrect value")
-        return jsonify(user)
+        return jsonify(username=user, year=year)
     except TypeError as te:
         return jsonify("Error {666}: " + str(te))
-
+    except ValueError:
+        return jsonify("Error: was no valid number")
 
