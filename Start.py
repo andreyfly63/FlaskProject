569f62a6e8b8a837fe20bbd8ddc75ae42e044b0c
andreyfly63 <andrey63ru@mail.ru>
2019-01-30 23:14:46 +0400

    fix bag and add func getall and clearall

--- Start.py
+++ Start.py
@@ -4,44 +4,54 @@ import flask
 import logging
 import os
 from flask import Flask, request, jsonify
+""" 
+todo - 
+1) добавить функцию "getall" - возвращает всё, что есть в файле  v
+2) добавить функцию "claerall" - удаляет всё, что есть в файле   v
+3) добавить мониторинг: 
+        /get_success - возвращает число успешных запросов
+        /get_error - возвращает число неуспешных запросов
+        /get_all - возвращает число запросов всего
+        /get_apm - возвращает среднее время ответа за последнюю секунду (average per minute) 
 
-
+совет - разобраться с "before request" и "before server start" в доках фласка
+удачи!
+"""
 app = Flask(__name__)
 
-""" add:
-    1)logging errors
-    2)save name and age
-    3)get name for age
 
-"""
+@app.route("/clearall")
+def clear_all():
+    logger = logging.getLogger("flask-project:/clearall")
+    logger.info("User clear all data in Example.json")
+    with open("Example.json", 'w'):
+        pass
+    return jsonify("data clear")
 
 
-def get_user_person_data(Username, Age):
-
-    user = {
-        'name': Username,
-        'age': Age
-    }
-    return user
+@app.route('/getall')
+def getall():
+    logger=logging.getLogger("flask-project:/getall")
+    logger.info("User get all information about names and ages")
+    if os.stat("Example.json").st_size == 0:
+        with open("Example.json", mode='w'):
+            pass
+        return jsonify("no data")
+    with open("Example.json", 'r') as file:
+        return jsonify(json.load(file))
 
 
 @app.route('/')
 def start_page():
-    """
-    Начальная страница
-    """
-    return jsonify("Hi, people! "
-                   "write \"add\", then add name and age, "
-                   "write \"get\", then get name if you write age or age if you write name")
+    return jsonify(r'''Hi, people! '''
+                   '''write \'add\', then add name and age, '''
+                   '''write \'get\', then get name if you write age or age if you write name''')
 
 
 @app.route('/add', methods=['GET'])
 def test():
-
-# !!!!!!! не работает если файл example пуст, надо чето придумать
-
     name_and_age_dict = {}
-    logger = logging.getLogger("flask-project.add")
+    logger = logging.getLogger("flask-project/add")
     try:
         name = request.args.get('name', 'Andrey')
         age = int(request.args.get('age', 23))
@@ -57,16 +67,12 @@ def test():
                 json.dump(name_and_age_dict, file)
         with open("Example.json", mode='r') as file:
             name_and_age_from_example = json.load(file)
-            print(name_and_age_dict)
+
             if name_and_age_from_example:
-                print(4)
                 if name_and_age_from_example.get(name) is None:
-                    print(5)
                     name_and_age_from_example[name] = age
             else:
-                print(6)
                 name_and_age_from_example[name] = age
-                print(7)
         with open("Example.json", mode='w', encoding='utf-8') as file:
             json.dump(name_and_age_from_example, file)
         return jsonify(name_and_age_dict)
@@ -79,35 +85,31 @@ def test():
 
 
 @app.route('/get', methods=['GET'])
-def get_name_for_age():
-    """ функция возвращает возраст если ввести имя или если ввести возраст то список имен с таким возрастом
-    """
+def get_info_by_parameter():
+    name = request.args.get("name")
+    age = request.args.get("age", 0)
+    print (type(age))
     name_for_print = []
-    logger = logging.getLogger("flask-project.get")
+    age = int(age)
+    if name is None and age == 0:
+        return "enter name OR age"
     try:
         with open('Example.json', 'r') as file:
-            name_and_age_from_example = json.load(file)
-        name = request.args.get('name', 'DefaultName')
-        age = int(request.args.get('age', -1))
-        pattern = r'[^a-zA-Z]+'
-        if re.findall(pattern, name):
-            raise ValueError
-# найти как сделать так чтоб пользователь мог вводить только возраст или имЯ
-        if name == 'DefaultName' and age == -1:
-            return "Write name or age"
-        elif name != 'DefaultName' and age == -1:
-            if name_and_age_from_example.get(name):
-                return jsonify(name_and_age_from_example.get(name))
+            if age == 0:
+                name_and_age_from_example = json.load(file)
+                if name_and_age_from_example.get(name):
+                        return jsonify(name_and_age_from_example.get(name))
+                else:
+                        return jsonify("value not found")
             else:
-                return jsonify("value not found")
-        else:
-            for key in name_and_age_from_example.keys():
-                if name_and_age_from_example.get(key) == age:
-                    name_for_print.append(key)
-            if name_for_print:
-                return jsonify(name_for_print)
-            else:
-                return jsonify("value not found")
+                name_and_age_from_example=json.load(file)
+                for key in name_and_age_from_example.keys():
+                    if name_and_age_from_example.get(key) == age:
+                        name_for_print.append(key)
+                if name_for_print:
+                    return jsonify(name_for_print)
+                else:
+                    return jsonify("value not found")
     except ValueError:
         logger.error('Error: User did not write value')
         return 'Error: User  write uncorrect value'
@@ -116,14 +118,9 @@ def get_name_for_age():
 if __name__ == "__main__":
     logger = logging.getLogger("flask-project")
     logger.setLevel(logging.INFO)
-
-    # create the logging file handler
     fh = logging.FileHandler("flask-project-log.log")
-
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
-
-    # add handler to logger object
     logger.addHandler(fh)
 
     app.run(host='0.0.0.0', port=8080, debug=True)
