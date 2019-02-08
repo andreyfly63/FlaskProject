a30436212d0b25ca22e76f4e5d7f977d4c1384fb
andreyfly63 <andrey63ru@mail.ru>
2019-02-09 00:42:25 +0400

    add monitoring

--- Start.py
+++ Start.py
@@ -1,9 +1,13 @@
 import json
 import re
+import time
+
 import flask
 import logging
 import os
-from flask import Flask, request, jsonify
+import utils
+from flask import Flask, request, jsonify, g
+
 """ 
 todo - 
 1) добавить функцию "getall" - возвращает всё, что есть в файле  v
@@ -17,32 +21,111 @@ todo -
 совет - разобраться с "before request" и "before server start" в доках фласка
 удачи!
 """
+
+_data_base = "Example.json"
+_pattern = re.compile("[^a-zA-Z]+")
+time_count = {}
+request_start_time = 0
+request_stop_time = 0
+error_count = 0
+success_count = 0
 app = Flask(__name__)
+
+
+@app.route("/geterror")
+def get_error():
+    global success_count
+    success_count += 1
+    return jsonify(error_count)
+
+
+@app.route("/getsuccess")
+def get_success():
+    global success_count
+    success_count += 1
+    return jsonify(success_count)
+
+
+@app.route("/getallrequest")
+def get_all_request():
+    global error_count, success_count
+    success_count += 1
+    return jsonify(error_count+success_count)
+
+
+@app.route("/getapm")
+def get_apm():
+    temp = 1
+    temp_time = 0
+    next_time = time.time()
+    global success_count, time_count, error_count
+    success_count += 1
+    for key in list(time_count.keys()):
+        if not list(time_count.keys()):
+            break
+        elif next_time-key < 60:
+            temp += 1
+            temp_time += time_count[key]
+            #print("temp=",temp,"time=",temp_time)
+        else:
+            print(next_time,"-",key,"=",next_time-key)
+            del time_count[key]
+    apm = temp_time/temp
+    if not time_count:
+        apm = next_time - request_start_time
+        return jsonify(apm)
+    #print("всего запросов=", success_count+error_count," запросов за минуту=", temp, "затраченое время=", temp_time)
+    return jsonify("apm=", apm)
+
+#    return jsonify(apm)
+
+
+@app.before_request
+def before_request():
+    global request_start_time
+    request_start_time = time.time()
+    print("start=", request_start_time)
+
+
+@app.after_request
+def after_request(response):
+    global time_count
+    global request_start_time
+    global request_stop_time
+    request_stop_time = time.time()
+#    r = request_stop_time-request_start_time
+    time_count[time.time()] = request_stop_time-request_start_time
+#    print("stop=", request_stop_time, "dev", r,"  ", time_count)
+    return response
 
 
 @app.route("/clearall")
 def clear_all():
+    global success_count
+    success_count += 1
     logger = logging.getLogger("flask-project:/clearall")
     logger.info("User clear all data in Example.json")
-    with open("Example.json", 'w'):
-        pass
+    os.remove(_data_base)
     return jsonify("data clear")
 
 
 @app.route('/getall')
 def getall():
+    global success_count
     logger=logging.getLogger("flask-project:/getall")
     logger.info("User get all information about names and ages")
-    if os.stat("Example.json").st_size == 0:
-        with open("Example.json", mode='w'):
-            pass
-        return jsonify("no data")
-    with open("Example.json", 'r') as file:
-        return jsonify(json.load(file))
+    try:
+        with open(_data_base, 'r') as file:
+            return jsonify(json.load(file))
+    except:
+        FileNotFoundError("exept")
+    return jsonify("no data")
 
 
 @app.route('/')
 def start_page():
+    global success_count
+    success_count += 1
     return jsonify(r'''Hi, people! '''
                    '''write \'add\', then add name and age, '''
                    '''write \'get\', then get name if you write age or age if you write name''')
@@ -50,22 +133,23 @@ def start_page():
 
 @app.route('/add', methods=['GET'])
 def test():
+    global success_count, error_count
     name_and_age_dict = {}
     logger = logging.getLogger("flask-project/add")
     try:
         name = request.args.get('name', 'Andrey')
         age = int(request.args.get('age', 23))
         name_and_age_dict[name] = age
-
-        pattern = r'[^a-zA-Z]+'
-        if re.findall(pattern, name):
+        time.sleep(3)
+        if re.findall(_pattern, name):
+            error_count += 1
             raise TypeError
         logger.info("User add name=%s and age=%s" % (name, age))
         name_and_age_dict[name] = age
-        if os.stat("Example.json").st_size == 0:
-            with open("Example.json", mode='w') as file:
+        if os.stat(_data_base).st_size == 0:
+            with open(_data_base, mode='w') as file:
                 json.dump(name_and_age_dict, file)
-        with open("Example.json", mode='r') as file:
+        with open(_data_base, mode='r') as file:
             name_and_age_from_example = json.load(file)
 
             if name_and_age_from_example:
@@ -73,33 +157,44 @@ def test():
                     name_and_age_from_example[name] = age
             else:
                 name_and_age_from_example[name] = age
-        with open("Example.json", mode='w', encoding='utf-8') as file:
+        with open(_data_base, mode='w', encoding='utf-8') as file:
             json.dump(name_and_age_from_example, file)
+            old_time = time.time()
+            new_time = time.time()
+            print("next time", new_time - old_time)
+        success_count += 1
         return jsonify(name_and_age_dict)
     except TypeError:
+
         logger.error('error(403): User add uncorrect name')
         return flask.abort(403)
     except ValueError:
+
         logger.error('error(404): User add uncorrect age')
         return flask.abort(404)
 
 
 @app.route('/get', methods=['GET'])
 def get_info_by_parameter():
+    global error_count, success_count
     name = request.args.get("name")
     age = request.args.get("age", 0)
-    print (type(age))
     name_for_print = []
     age = int(age)
+    #data_from_data_base = utils.get_data_from_file(name, age, _data_base)
+    #print(data_from_data_base)
     if name is None and age == 0:
+        error_count += 1
         return "enter name OR age"
     try:
-        with open('Example.json', 'r') as file:
+        with open(_data_base, 'r') as file:
             if age == 0:
                 name_and_age_from_example = json.load(file)
                 if name_and_age_from_example.get(name):
+                        success_count += 1
                         return jsonify(name_and_age_from_example.get(name))
                 else:
+                        error_count += 1
                         return jsonify("value not found")
             else:
                 name_and_age_from_example=json.load(file)
@@ -107,15 +202,19 @@ def get_info_by_parameter():
                     if name_and_age_from_example.get(key) == age:
                         name_for_print.append(key)
                 if name_for_print:
+                    success_count += 1
                     return jsonify(name_for_print)
                 else:
+                    error_count += 1
                     return jsonify("value not found")
     except ValueError:
+        error_count += 1
         logger.error('Error: User did not write value')
         return 'Error: User  write uncorrect value'
 
 
 if __name__ == "__main__":
+
     logger = logging.getLogger("flask-project")
     logger.setLevel(logging.INFO)
     fh = logging.FileHandler("flask-project-log.log")
